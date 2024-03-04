2a7c81a2fe33ec3802ef26bc71efda88ae368fca
nath <161667729+nataliaDaire@users.noreply.github.com>
2024-03-04 15:58:16 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-
+<?php
+//Crear objeto
+//Crear un array
+$platillo = [
+  "nombre" => "Sushi",
+  "precio" => $150,
+  "porcion" => "Dos Personas",
+  "de donde es originario" => "Japon",
+  "ingredientes" => "Arroz,Alga,Camaron,Queso Philadelfia",
+  "tiempo de preparacion" => "Veinte Minutos",
+  "dias que se sirve" => "Lunes,Miercoles,Viernes,Sabado y Domingo",
+  "bebida de acompañamiento" => "Te Helado",
